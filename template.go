5dcc750b2c54d5ada3ed067ebe6cac5142c9e320
Antony Karounis <nullfocus@gmail.com>
2024-12-20 19:21:25 -0500

    improved define handling

--- template.go
+++ template.go
@@ -13,20 +13,26 @@ import (
 	"github.com/nullfocus/govalidtemple"
 )
 
+var (
+	BASE = "base.html"
+)
+
 type TemplateManagerOptions struct {
-	RootPath        string
-	IncludePath     string
-	WatchForChanges bool
+	RootPath    string
+	IncludePath string
 }
 
 // -----------------------------------
 
 type TemplateManager struct {
-	rootTemplate *template.Template
+	storedTemplates map[string]*template.Template
+	baseExists      bool
 }
 
 func NewTemplateManager(options TemplateManagerOptions) (*TemplateManager, error) {
-	rootTemplate := template.New("root")
+	tm := TemplateManager{
+		storedTemplates: make(map[string]*template.Template),
+	}
 
 	// TODO: validate options
 
@@ -36,24 +42,20 @@ func NewTemplateManager(options TemplateManagerOptions) (*TemplateManager, error
 
 	// load includes
 	absIncludePath, err := filepath.Abs(options.IncludePath)
+	if err != nil {
+		return nil, err
+	}
 	fmt.Println("loading includes:")
+	includes, err := template.New("root").ParseGlob(filepath.Join(absIncludePath, "*"))
 	if err != nil {
 		return nil, err
 	}
-	err = filepath.WalkDir(absIncludePath, func(path string, d fs.DirEntry, err error) error {
-		if !d.IsDir() && err == nil && d.Type().IsRegular() {
-			fmt.Println("  " + d.Name())
-			//insert each include into the root
-			rootTemplate, err = rootTemplate.ParseFiles(path)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
+
+	base := includes.Lookup(BASE)
+	if base != nil {
+		tm.baseExists = true
 	}
+
 	// load templates
 	absRootPath, err := filepath.Abs(options.RootPath)
 	fmt.Println("loading templates:")
@@ -62,57 +64,71 @@ func NewTemplateManager(options TemplateManagerOptions) (*TemplateManager, error
 	}
 	err = filepath.WalkDir(absRootPath, func(path string, d fs.DirEntry, err error) error {
 		if !d.IsDir() && err == nil && d.Type().IsRegular() {
-			//parse into new template
-			newTemplate, err := template.ParseFiles(path)
-			if err != nil {
-				return err
-			}
 			relativePath, err := filepath.Rel(absRootPath, path)
 			if err != nil {
 				return err
 			}
-			fmt.Println("  " + relativePath)
-			//merge into root
-			rootTemplate, err = rootTemplate.AddParseTree(relativePath, newTemplate.Tree)
+
+			newTemplate, err := includes.Clone()
 			if err != nil {
 				return err
 			}
+
+			newTemplate, err = newTemplate.ParseFiles(path)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("  " + relativePath)
+
+			tm.storedTemplates[relativePath] = newTemplate
 		}
 		return nil
 	})
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println()
 
-	return &TemplateManager{
-		rootTemplate: rootTemplate,
-	}, nil
+	return &tm, nil
 }
 
 func (tm *TemplateManager) GetExecutor(templatePath string, exampleModel any) (*TemplateExecutor, error) {
-	tmpl := tm.rootTemplate.Lookup(templatePath)
+	tmpl := tm.storedTemplates[templatePath]
 	if tmpl == nil {
 		return nil, errors.New("couldn't find template: " + templatePath)
 	}
 
-	err := govalidtemple.ValidateViewModel(exampleModel, tmpl, templatePath)
+	var err error = nil
+	if tm.baseExists {
+		err = govalidtemple.ValidateViewModel(exampleModel, tmpl, BASE)
+	} else {
+		err = govalidtemple.ValidateViewModel(exampleModel, tmpl, templatePath)
+	}
 	if err != nil {
 		return nil, errors.New("couldn't validate view model: " + err.Error())
+	} else {
+		fmt.Println("validated template")
 	}
 
 	return &TemplateExecutor{
 		targetTemplate: tmpl,
+		templateName:   templatePath,
+		baseExists:     tm.baseExists,
 	}, nil
 }
 
 func (tm *TemplateManager) GetTemplate(templatePath string, exampleModel any) (*template.Template, error) {
-	tmpl := tm.rootTemplate.Lookup(templatePath)
+	tmpl := tm.storedTemplates[templatePath]
 	if tmpl == nil {
 		return nil, errors.New("couldn't find template: " + templatePath)
 	}
 
-	err := govalidtemple.ValidateViewModel(exampleModel, tmpl, templatePath)
+	var err error = nil
+	if tm.baseExists {
+		err = govalidtemple.ValidateViewModel(exampleModel, tmpl, BASE)
+	} else {
+		err = govalidtemple.ValidateViewModel(exampleModel, tmpl, templatePath)
+	}
 	if err != nil {
 		return nil, errors.New("couldn't validate view model: " + err.Error())
 	}
@@ -154,20 +170,36 @@ func (tm *TemplateManager) ExecutorRoute(
 
 type TemplateExecutor struct {
 	targetTemplate *template.Template
+	templateName   string
+	baseExists     bool
 }
 
 func (tm TemplateExecutor) ExecuteToString(data any) (string, error) {
 	buffer := bytes.NewBufferString("")
-	err := tm.targetTemplate.Execute(buffer, data)
+	var err error = nil
+	if tm.baseExists {
+		err = tm.targetTemplate.ExecuteTemplate(buffer, BASE, data)
+	} else {
+		err = tm.targetTemplate.ExecuteTemplate(buffer, tm.templateName, data)
+	}
+
 	if err != nil {
 		return "", fmt.Errorf("error executing template [%v]: %v", tm.targetTemplate.Name(), err.Error())
 	}
+
+	fmt.Println(tm.targetTemplate.Tree)
 
 	return buffer.String(), nil
 }
 
 func (tm TemplateExecutor) ExecuteToWriter(writer io.Writer, data any) error {
-	err := tm.targetTemplate.Execute(writer, data)
+	var err error = nil
+	if tm.baseExists {
+		err = tm.targetTemplate.ExecuteTemplate(writer, BASE, data)
+	} else {
+		err = tm.targetTemplate.ExecuteTemplate(writer, tm.templateName, data)
+	}
+
 	if err != nil {
 		return fmt.Errorf("error executing template [%v]: %v", tm.targetTemplate.Name(), err.Error())
 	}
